eabd88ba18fdec229f50adc7fec0cafc1af6ac7c
li-shuai <506507881@qq.com>
2025-06-02 23:52:29 +0800

    feat:config.ts

---
+++ packages/core/vite.es.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    outDir: 'dist/es',
+    lib: {
+      entry: resolve(__dirname, './index.ts'),
+      name: 'NsElement',
+      fileName: 'index',
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: [
+        'vue',
+        '@fortawesome/fontawesome-svg-core',
+        '@fortawesome/free-solid-svg-icons',
+        '@fortawesome/vue-fontawesome',
+        '@popperjs/core',
+        'async-validator',
+      ],
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === 'style.css') return 'index.css'
+          return assetInfo.name as string
+        },
+      },
+    },
+  },
+})
