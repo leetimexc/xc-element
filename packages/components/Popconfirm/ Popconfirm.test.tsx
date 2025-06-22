305d540aa147c4863977c0d362f84bb77ac5617e
li-shuai <506507881@qq.com>
2025-06-23 01:44:27 +0800

    feat: Popconfirm.test.tsx

--- packages/components/Popconfirm/ Popconfirm.test.tsx
+++ packages/components/Popconfirm/ Popconfirm.test.tsx
@@ -1,40 +1,40 @@
-import { describe, it,/* test, */expect,/* vi, beforeEach */} from "vitest";
+import { describe, it,test, expect,vi, beforeEach } from "vitest";
 import { mount } from "@vue/test-utils";
-// import { withInstall } from "@xc-element/utils";
+import { withInstall } from "@xc-element/utils";
 import { each, get } from "lodash-es";
 import type { PopconfirmProps } from "./types";
-// import { XcPopconfirm } from ".";
+import { XcPopconfirm } from ".";
 
-// import Popconfirm from "./Popconfirm.vue";
+import Popconfirm from "./Popconfirm.vue";
 
-// const onConfirm = vi.fn();
-// const onCancel = vi.fn();
+const onConfirm = vi.fn();
+const onCancel = vi.fn();
 
-// describe("Popconfirm/index.ts", () => {
-//   // 测试 withInstall 函数是否被正确应用
-//   it("should be exported with withInstall()", () => {
-//     expect(ErPopconfirm.install).toBeDefined();
-//   });
+describe("Popconfirm/index.ts", () => {
+  // 测试 withInstall 函数是否被正确应用
+  it("should be exported with withInstall()", () => {
+    expect(XcPopconfirm.install).toBeDefined();
+  });
 
-//   // 测试 Popconfirm 组件是否被正确导出
-//   it("should be exported Popconfirm component", () => {
-//     expect(ErPopconfirm).toBe(Popconfirm);
-//   });
+  // 测试 Popconfirm 组件是否被正确导出
+  it("should be exported Popconfirm component", () => {
+    expect(XcPopconfirm).toBe(Popconfirm);
+  });
 
-//   // 可选：测试 withInstall 是否增强了 Popconfirm 组件的功能
-//   test("should enhance Popconfirm component", () => {
-//     const enhancedPopconfirm = withInstall(Popconfirm);
-//     expect(enhancedPopconfirm).toBe(ErPopconfirm);
-//     // 这里可以添加更多测试，确保 withInstall 增强了组件的特定功能
-//   });
+  // 可选：测试 withInstall 是否增强了 Popconfirm 组件的功能
+  test("should enhance Popconfirm component", () => {
+    const enhancedPopconfirm = withInstall(Popconfirm);
+    expect(enhancedPopconfirm).toBe(XcPopconfirm);
+    // 这里可以添加更多测试，确保 withInstall 增强了组件的特定功能
+  });
 
-//   // 可选：如果你的 withInstall 函数有特定的行为或属性，确保它们被正确应用
-//   test("should apply specific enhancements", () => {
-//     const  enhancedPopconfirm  = withInstall(Popconfirm);
-//     // 例如，如果你的 withInstall 增加了一个特定的方法或属性
-//     expect(enhancedPopconfirm).toHaveProperty("install");
-//   });
-// });
+  // 可选：如果你的 withInstall 函数有特定的行为或属性，确保它们被正确应用
+  test("should apply specific enhancements", () => {
+    const  enhancedPopconfirm  = withInstall(Popconfirm);
+    // 例如，如果你的 withInstall 增加了一个特定的方法或属性
+    expect(enhancedPopconfirm).toHaveProperty("install");
+  });
+});
 
 // 测试组件是否能够接收所有 props
 describe("Popconfirm.vue", () => {
@@ -51,10 +51,10 @@ describe("Popconfirm.vue", () => {
     width: 200,
   } as PopconfirmProps;
 
-  // beforeEach(() => {
-  //   vi.useFakeTimers();
-  //   vi.clearAllMocks();
-  // });
+  beforeEach(() => {
+    vi.useFakeTimers();
+    vi.clearAllMocks();
+  });
 
   it("should accept all props", () => {
     const wrapper = mount(Popconfirm, {
@@ -78,48 +78,48 @@ describe("Popconfirm.vue", () => {
     });
 
     expect(wrapper.text()).toContain(slotContent);
-  // });
+  });
 
-  // test("popconfirm emits", async () => {
-  //   const wrapper = mount(() => (
-  //     <div>
-  //       <div id="outside"></div>
-  //       <Popconfirm
-  //         title="Test Title"
-  //         hideIcon={true}
-  //         onConfirm={onConfirm}
-  //         onCancel={onCancel}
-  //       >
-  //         <button id="trigger">trigger</button>
-  //       </Popconfirm>
-  //     </div>
-  //   ));
-  //   const triggerArea = wrapper.find("#trigger");
-  //   expect(triggerArea.exists()).toBeTruthy();
+  test("popconfirm emits", async () => {
+    const wrapper = mount(() => (
+      <div>
+        <div id="outside"></div>
+        <Popconfirm
+          title="Test Title"
+          hideIcon={true}
+          onConfirm={onConfirm}
+          onCancel={onCancel}
+        >
+          <button id="trigger">trigger</button>
+        </Popconfirm>
+      </div>
+    ));
+    const triggerArea = wrapper.find("#trigger");
+    expect(triggerArea.exists()).toBeTruthy();
 
-  //   triggerArea.trigger("click");
-  //   await vi.runAllTimers();
+    triggerArea.trigger("click");
+    await vi.runAllTimers();
 
-  //   // 弹出层是否出现
-  //   expect(wrapper.find(".er-popconfirm").exists()).toBeTruthy();
-  //   const confirmButton = wrapper.find(".er-popconfirm__confirm");
-  //   expect(confirmButton.exists()).toBeTruthy();
+    // 弹出层是否出现
+    expect(wrapper.find(".xc-popconfirm").exists()).toBeTruthy();
+    const confirmButton = wrapper.find(".xc-popconfirm__confirm");
+    expect(confirmButton.exists()).toBeTruthy();
 
-  //   confirmButton.trigger("click");
-  //   await vi.runAllTimers();
-  //   expect(wrapper.find(".er-popconfirm").exists()).toBeFalsy();
-  //   expect(onConfirm).toBeCalled();
+    confirmButton.trigger("click");
+    await vi.runAllTimers();
+    expect(wrapper.find(".xc-popconfirm").exists()).toBeFalsy();
+    expect(onConfirm).toBeCalled();
 
-  //   triggerArea.trigger("click");
-  //   await vi.runAllTimers();
-  //   expect(wrapper.find(".er-popconfirm").exists()).toBeTruthy();
-  //   const cancelButton = wrapper.find(".er-popconfirm__cancel");
-  //   expect(cancelButton.exists()).toBeTruthy();
+    triggerArea.trigger("click");
+    await vi.runAllTimers();
+    expect(wrapper.find(".xc-popconfirm").exists()).toBeTruthy();
+    const cancelButton = wrapper.find(".xc-popconfirm__cancel");
+    expect(cancelButton.exists()).toBeTruthy();
 
-  //   await vi.runAllTimers();
-  //   cancelButton.trigger("click");
-  //   await vi.runAllTimers();
-  //   expect(wrapper.find(".er-popconfirm").exists()).toBeFalsy();
-  //   expect(onCancel).toBeCalled();
-  // });
+    // await vi.runAllTimers();
+    cancelButton.trigger("click");
+    await vi.runAllTimers();
+    expect(wrapper.find(".xc-popconfirm").exists()).toBeFalsy();
+    expect(onCancel).toBeCalled();
+  });
 });
