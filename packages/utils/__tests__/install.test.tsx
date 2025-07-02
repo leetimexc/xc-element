8c9fb0a34b4191ea941606c974810da6675eb20a
li-shuai <506507881@qq.com>
2025-07-03 01:29:23 +0800

    feat:报错解决

--- packages/utils/__tests__/install.test.tsx
+++ packages/utils/__tests__/install.test.tsx
@@ -1,54 +1,39 @@
-import { describe, expect, it } from 'vitest'
-import { mount } from '@vue/test-utils'
-import { defineComponent, createApp } from 'vue'
-import { withInstall, makeInstaller } from '../install'
+import { describe, expect, it } from "vitest";
+import { mount } from "@vue/test-utils";
+import { defineComponent, createApp } from "vue";
+
+import { withInstall } from "../install";
 
 const AppComp = defineComponent({
   setup() {
-    return () => <div>app</div>
+    return () => <div>App</div>;
   },
 })
 
-const componentA = withInstall(
-  defineComponent({
-    name: 'componentA',
-    setup() {
-      return () => <div>test</div>
-    }
-  })
-)
+const compA = withInstall(defineComponent({
+  name:'CompA',
+  setup() {
+    return () => <div>CompA</div>;
+  },
+}))
 
-const componentB = withInstall(
-  defineComponent({
-    name: 'componentB',
-    setup() {
-      return () => <div>test2</div>
-    },
-  })
-)
+const compB = withInstall(defineComponent({
+  name:'CompB',
+  setup() {
+    return () => <div>CompB</div>;
+  },
+}))
 
-describe('install', () => {
-  it('withInstall should be worked', () => {
-    const wrapper = mount(() => <div id="app"></div>)
+describe('install',()=>{
+  it('withInstall should be worked',()=>{
+    const wrapper = mount(()=><div id="app"></div>)
     const app = createApp(AppComp)
 
-    app.use(componentA).mount(wrapper.element)
+    app.use(compA).mount(wrapper.element)
 
-    expect(componentA.install).toBeDefined()
-    expect(componentB.install).toBeDefined()
-    expect(app._context.components['componentA']).toBeTruthy()
-    expect(app._context.components['componentB']).toBeFalsy()
-  })
-
-  it('makeInstaller should be worked', () => {
-    const wrapper = mount(() => <div id="app"></div>)
-    const app = createApp(AppComp)
-    const installer = makeInstaller([componentA, componentB])
-
-    app.use(installer).mount(wrapper.element)
-
-    expect(installer).toBeDefined()
-    expect(wrapper.findComponent(componentA)).toBeTruthy()
-    expect(wrapper.findComponent(componentB)).toBeTruthy()
+    expect(compA.install).toBeDefined()
+    expect(compB.install).toBeDefined()
+    expect(app._context.components['CompA']).toBeTruthy()
+    expect(app._context.components['CompB']).toBeFalsy()
   })
 })
